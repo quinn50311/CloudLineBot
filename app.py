25bb2e3c62737b606a448990b68ed57df576fc88
quinn <j10j10j10quinn51@gmail.com>
2018-11-28 15:17:21 +0800

    '測試'

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from linebot.models import *
 app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
-
+#87
 # config
 line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 handler = WebhookHandler(config['line_bot']['Channel_Secret'])
