95f2381406f92d0d306e038237a72abb52bd6ede
quinn <j10j10j10quinn51@gmail.com>
2018-11-29 01:43:24 +0800

    test

--- app.py
+++ app.py
@@ -106,17 +106,17 @@ def handle_message(event):
     else:
         message = TextSendMessage(text="請輸入正確縣市名稱")
 
-@handler.add(MessageEvent, message=StickerMessage)	
-def handle_sticker_message(event):
-    print("package_id:", event.message.package_id)
-    print("sticker_id:", event.message.sticker_id)
-    sticker_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 100, 101, 102, 103, 104, 105, 106,
-                   107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
-                   126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 401, 402]
-    index_id = random.randint(0, len(sticker_ids) - 1)
-    sticker_id = str(sticker_ids[index_id])
-    sticker_message = StickerSendMessage(package_id='1', sticker_id=sticker_id)
-    line_bot_api.reply_message(event.reply_token, sticker_message)
+# @handler.add(MessageEvent, message=StickerMessage)	
+# def handle_sticker_message(event):
+    # print("package_id:", event.message.package_id)
+    # print("sticker_id:", event.message.sticker_id)
+    # sticker_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 100, 101, 102, 103, 104, 105, 106,
+                   # 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
+                   # 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 401, 402]
+    # index_id = random.randint(0, len(sticker_ids) - 1)
+    # sticker_id = str(sticker_ids[index_id])
+    # sticker_message = StickerSendMessage(package_id='1', sticker_id=sticker_id)
+    # line_bot_api.reply_message(event.reply_token, sticker_message)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
