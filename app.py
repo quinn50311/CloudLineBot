e8cbb66cb131822a975a2e1ff34ce4816f2d6a27
quinn <j10j10j10quinn51@gmail.com>
2018-12-01 14:24:26 +0800

    test

--- app.py
+++ app.py
@@ -98,12 +98,13 @@ def train_time(train_stop1, train_stop2):
         for i in range(4, len(time_tag), 10):
             if str(time) == str(time_tag[time_start])[4:6] or str(time1) == str(time_tag[time_start])[4:6]:
                 index = time_tag.index(time_tag[time_start])
-                print(time_tag[time_start])
                 if str(time_tag[index - 4])[6] != "<":
                     all = str(time_tag[index - 4])[4:7] + " " + str(time_tag[index])[4:9] + " " + str(time_tag[index + 1])[4:9]
                 else:
                     all = str(time_tag[index - 4])[4:6] + "號" + " " + str(time_tag[index])[4:9] + " " + str(time_tag[index + 1])[4:9]
                 content = content + all + "\n"
+            else:
+                print(time_tag[time_start])
             time_start = time_start + 10
     return content
     
