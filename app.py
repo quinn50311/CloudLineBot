334484c74342f56e2d0d57b495a3bb61c3d8f961
quinn50311 <j10j10j10quinn51@gmail.com>
2019-11-02 20:40:57 +0800

    test

--- app.py
+++ app.py
@@ -128,6 +128,7 @@ def get_html(url):
 	try:
 		response = requests.get(url, headers=headers)
 		if response.status_code == 200:
+			print(response.text)
 			return response.text
 		else:
 			print('請求錯誤狀態碼:', response.status_code)
